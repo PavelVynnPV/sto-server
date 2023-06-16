1e9445221e49894e4cc8dca02970887eb5106e78
Pavel <92670880+PavelVynnPV@users.noreply.github.com>
2023-06-16 15:31:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const Comment = require('./models/Comments'); // Подключение моде
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "https://pavelvynnpv.github.io");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
