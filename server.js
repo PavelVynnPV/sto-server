3edf52dfe6539a9c7445aba0b77a70df40c22d77
Pavel <pavel.vynn@gmail.com>
2023-09-02 19:04:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Comment = require("./models/Comments"); // Подключение моде
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
